0cb0c8a4b43b1d0ffa8981dbc0105a611d90fa9c
Julian Cole <juliancole@Julians-MacBook-Pro.local>
2021-01-28 19:21:19 -0800

    feature: server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const usersRouter = require('./user/users-router');
+// const Router = require('./event_planner/event-router');
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+// server.use(eventRouter);
+// server.use(usersRouter);
+
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
+module.exports = server;
